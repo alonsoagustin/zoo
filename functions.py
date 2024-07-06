a2e0557efd04d48040fea32e73f3d46eff4e5f57
Agustin Alonso <alonsoiagustin@gmail.com>
2024-07-07 01:07:03 +0200

    feat: :sparkles: Add calc_price function.
    Small modifications to the other functions to adapt them to the new function.

--- functions.py
+++ functions.py
@@ -1,12 +1,13 @@
+from variables import price_list
+
 def request_age():
     while True:
         try:
             user_response = input("Ingrese una edad: ")
             if user_response == "":
-                break
+                return False
             else:
-                is_valid(user_response)
-                return user_response
+                return is_valid(user_response)
         except Exception as e:
             print(f" Error inesperado: {e}")
 
@@ -15,7 +16,19 @@ def is_valid(user_response:str)->bool:
         number = int(user_response)
         if number < 0:
             raise ValueError(f"Usted ingresó una edad inválida: {user_response}")
-        return True
+        return number
     except ValueError as e:
         print(f"Error: {e}. Por favor, ingrese una edad válida.")
         return False
+
+def calc_price():
+    age = request_age()
+    total = 0
+    while age:
+        for key,value in price_list.items():
+            if age < value['edad']:
+                value['contador'] += 1
+                total += value['precio']
+                break
+        age = request_age()
+    return None
