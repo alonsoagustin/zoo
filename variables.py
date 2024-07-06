6f873a6b05035c62093dd7faad3745fe51de5965
Agustin Alonso <alonsoiagustin@gmail.com>
2024-07-07 00:28:57 +0200

    refactor: :truck: Move dictionary to a new file.

---
+++ variables.py
@@ -0,0 +1,6 @@
+price_list = {
+    'Bebés':{'precio': 0,'edad': 3,'contador': 0},
+    'Niños':{'precio': 14,'edad': 13,'contador': 0},
+    'Adultos':{'precio': 23,'edad': 65,'contador': 0},
+    'Jubilados':{'precio': 18,'edad': float('inf'),'contador': 0}
+}
